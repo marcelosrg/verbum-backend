ff9037b05db1fd64df7461a3dece785d298c7480
marcelosrg <marcelohrs2005@gmail.com>
2026-03-24 15:04:14 -0300

    feat: foi adicionada a implementação do meu caso de uso de criar usuario

--- src/main/java/com/verbum/api/core/useCases/CreateUsersUseCaseImpl.java
+++ src/main/java/com/verbum/api/core/useCases/CreateUsersUseCaseImpl.java
@@ -1,4 +1,17 @@
 package com.verbum.api.core.useCases;
 
-public class CreateUsersUseCaseImpl {
+import com.verbum.api.core.domain.User;
+import com.verbum.api.core.gateway.UsersGateway;
+
+public class CreateUsersUseCaseImpl implements CreateUsersUseCase {
+
+    private UsersGateway usersGateway;
+
+    public CreateUsersUseCaseImpl(UsersGateway usersGateway) {
+        this.usersGateway = usersGateway;
+    }
+    @Override
+    public User execute(User user) {
+        return usersGateway.createUser(user);
+    }
 }
