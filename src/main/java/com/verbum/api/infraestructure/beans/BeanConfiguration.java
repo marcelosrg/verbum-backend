152948c5290d991b83d0432e0af03f93cc19a6f5
marcelosrg <marcelohrs2005@gmail.com>
2026-03-24 15:04:53 -0300

    chore: foi criado uma configuração para resolver injeção de dependencia entre camada de core e infra

--- src/main/java/com/verbum/api/infraestructure/beans/BeanConfiguration.java
+++ src/main/java/com/verbum/api/infraestructure/beans/BeanConfiguration.java
@@ -1,4 +1,15 @@
 package com.verbum.api.infraestructure.beans;
 
+import com.verbum.api.core.gateway.UsersGateway;
+import com.verbum.api.core.useCases.CreateUsersUseCase;
+import com.verbum.api.core.useCases.CreateUsersUseCaseImpl;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
 public class BeanConfiguration {
+
+    @Bean
+    public CreateUsersUseCase createUsersUseCase(UsersGateway usersGateway) {
+        return new CreateUsersUseCaseImpl(UsersGateway);
+    }
 }
